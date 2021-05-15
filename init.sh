7f5372f92c3dcd19de942ce440807e79da1fed35
Alexandr Timofeev <tim31al@gmail.com>
2021-05-15 14:05:18 +0300

    scripts 3

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker-compose up -d && \
-docker-compose run app composer install && php vendor/bin/doctrine orm:schema-tool:create --force
+docker-compose run app composer install && php vendor/bin/doctrine orm:schema-tool:create
 docker-compose down
